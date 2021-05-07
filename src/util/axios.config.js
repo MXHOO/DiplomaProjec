0315b2cc3923d74d3fe4273ab34a43283d55662f
孟晓慧 <mengxiaohui@renrenche.com>
2021-05-07 19:38:37 +0800

    refactor: router

--- src/util/axios.config.js
+++ src/util/axios.config.js
@@ -10,7 +10,7 @@ const service = axios.create({
 })
 
 service.interceptors.request.use(config => {
-  // console.log('config', config)
+  // console.log('请求之前', router.push)
   if (!config.headers.token && !window.localStorage.getItem('token')) {
     setTimeout(() => { router.push({ path: '/login' }) })
   } else {
@@ -23,13 +23,14 @@ service.interceptors.request.use(config => {
 })
 
 service.interceptors.response.use(response => {
+  // console.log('拦截', router.push)
   const { data } = response
   if (data.code === 0) {
     return Promise.resolve(data)
   } else if (data.code === 1003 || data.code === 1002) {
     vue.prototype.$message.error(data.msg)
     setTimeout(_ => {
-      router.push({ path: '/login' })
+      vue.prototype.$$router.push({ path: '/login' })
       localStorage.removeItem('token')
     })
     return Promise.reject(data)
